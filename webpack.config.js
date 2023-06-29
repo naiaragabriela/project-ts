75f7bdc4fa834f369bed14edbcc902bc9b64a10f
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-29 19:24:46 -0300

    chore: correct webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/webpack/index.ts',
+  entry: './src/folderwebpack/index.ts',
   module: {
     rules: [
       {
