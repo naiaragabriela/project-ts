2a1a075134554b79a9d65b62aaf987cc5372223a
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-30 21:02:32 -0300

    feat: folders abstract and heranca

---
+++ src/objects/interfaces/interfaces.ts
@@ -0,0 +1,35 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'Naiara',
+  sobrenome: 'Agora tá ok',
+};
+
+const pessoa = new Pessoa('Naiara', 'Gabriela');
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
